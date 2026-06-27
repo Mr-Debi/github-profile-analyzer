3a6450fc1b820120a2544dd93bc03f559dde515a
Debidutta Behera <lipundebidutta.db@gmail.com>
2026-06-28 00:33:03 +0530

    FIX

--- config/db.js
+++ config/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error("Database connection failed:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
-});
-
-module.exports = connection;
+module.exports = pool;
